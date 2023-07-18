deba0a8cd3ea1f87b7139aeee7e5d4f44892c738
Aashish Katwal <aashiskutwal5@gmail.com>
2023-07-18 14:54:01 +0545

    Autowirting using XML

---
+++ springcore/src/main/java/com/springcore/autowire/App.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package com.springcore.autowire;
+
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * @author Aashish
+ *
+ */
+public class App {
+
+	public static void main(String[] args) {
+		AbstractApplicationContext ac = new ClassPathXmlApplicationContext("com/springcore/autowire/autowireConfig.xml");
+		
+		Student std1 = ac.getBean("Student1", Student.class); //Type casting not required.
+		Student std2 = ac.getBean("Student2", Student.class); //Type casting not required.
+		
+		System.out.println(std1);
+		System.out.println(std2);
+		
+		ac.close();
+
+	}
+
+}
---
+++ springcore/src/main/java/com/springcore/autowire/Student.java
@@ -0,0 +1,49 @@
+package com.springcore.autowire;
+
+public class Student {
+
+	private int roll;
+	private String majorCourse;
+	private Person person;
+
+	public Student() {
+		super();
+	}
+
+	public Student(int roll, String majorCourse, Person person) {
+		super();
+		this.roll = roll;
+		this.majorCourse = majorCourse;
+		this.person = person;
+	}
+
+	public int getRoll() {
+		return roll;
+	}
+
+	public void setRoll(int roll) {
+		this.roll = roll;
+	}
+
+	public String getMajorCourse() {
+		return majorCourse;
+	}
+
+	public void setMajorCourse(String majorCourse) {
+		this.majorCourse = majorCourse;
+	}
+
+	public Person getPerson() {
+		return person;
+	}
+
+	public void setPerson(Person person) {
+		this.person = person;
+	}
+
+	@Override
+	public String toString() {
+		return "Student [roll=" + roll + ", majorCourse=" + majorCourse + ", person=" + person + "]";
+	}
+
+}
